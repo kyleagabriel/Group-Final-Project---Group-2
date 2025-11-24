48f9609118a4e7e8755c2676ba647eddb6efbd5c
caitlincheung1 <caitlin.cheung@student.ateneo.edu>
2025-11-24 22:44:25 +0800

    Update settings.py

--- pitstop/pitstop/settings.py
+++ pitstop/pitstop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "product_list"
 LOGOUT_REDIRECT_URL = "login"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+}
